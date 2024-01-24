c68bf87c0db181eaebbc84bead57b264bf0f9d6a
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-24 10:38:19 +0530

    Add files via upload

---
+++ f_str.py
@@ -0,0 +1,13 @@
+# f string in py
+# 1
+num = 12
+str = f"value of num is {num}"
+print(str)
+
+#2
+num = 15
+str = f"value of num is {num}"
+print(str)
+print(f"value of num is {num} scripted")
+
+
